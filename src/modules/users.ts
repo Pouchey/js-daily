93a42bf6320b1e810e185b89f3e76907f047b7d1
Théophile <theophilerouchet@gmail.com>
2023-02-27 18:22:52 +0100

    update username

--- src/modules/users.ts
+++ src/modules/users.ts
@@ -5,12 +5,11 @@ import { UserType } from "../types/player";
 export const getUsers = async (users: Collection<string, GuildMember>) => {
   const usersName: UserType[] = [];
 
-  console.log(users);
   users.forEach((user) => {
     return usersName.push(
       {
         id: user.id,
-        name: user.displayName,
+        name: user.user.username,
       });
   });
   return usersName;
